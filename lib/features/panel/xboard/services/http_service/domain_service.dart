60f8076025e95026980c48dd32186cd49bcb269d
hovans <honven20@gmai.com>
2024-10-30 19:46:51 +0800

    yt-oss

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -7,16 +7,17 @@ class DomainService {
   // 定义多个 ossDomain 地址
   static const List<String> ossDomains = [
     'https://vdawa.oss-cn-hongkong.aliyuncs.com/youtu/config.json',
-    'https://youtu.ossconfig/config.json',
-    'https://youtu.ossconfig2/config.json',
-    'https://youtu.ossconfig3/config.json',
-    'https://youtu.ossconfig4/config.json',
-    'https://youtu.ossconfig5/config.json',
-    'https://youtu.ossconfig6/config.json',
-    'https://youtu.ossconfig7/config.json',
-    'https://youtu.ossconfig8/config.json',
-    'https://youtu.ossconfig9/config.json',
-    'https://youtu.ossconfig10/config.json'
+    'https://youtu.ossconfig.top/config.json',
+    'https://youtu.ossconfig1.top/config.json',
+    'https://youtu.ossconfig2.top/config.json',
+    'https://youtu.ossconfig3.top/config.json',
+    'https://youtu.ossconfig4.top/config.json',
+    'https://youtu.ossconfig5.top/config.json',
+    'https://youtu.ossconfig6.top/config.json',
+    'https://youtu.ossconfig7.top/config.json',
+    'https://youtu.ossconfig8.top/config.json',
+    'https://youtu.ossconfig9.top/config.json',
+    'https://youtu.ossconfig10.top/config.json'
   ];
 
   // 从多个 ossDomain 中获取 JSON 并挑选一个可以正常访问的域名
