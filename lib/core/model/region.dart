2aa0d3c2999d7218d537fe52a355b0694e966cdd
hovans <honven20@gmai.com>
2025-01-06 23:33:06 +0800

    去除多余语言和地区2

--- lib/core/model/region.dart
+++ lib/core/model/region.dart
@@ -1,13 +1,7 @@
 import 'package:hiddify/core/localization/translations.dart';
 
 enum Region {
-  ir,
   cn,
-  ru,
-  af,
-  id,
-  tr,
-  br,
   other;
 
   String present(TranslationsEn t) => switch (this) {
